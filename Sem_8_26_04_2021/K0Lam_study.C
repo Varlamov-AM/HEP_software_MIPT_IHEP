3654922e4268a39797ae7face3fb9a26fe1e2183
Antony Varlamov <varlamov.AM@phystech.edu>
2021-04-26 10:39:28 +0300

    Added materials to eighth seminar

--- Sem_8_26_04_2021/K0Lam_study.C
+++ Sem_8_26_04_2021/K0Lam_study.C
@@ -30,10 +30,17 @@ void K0Lam_study::Loop()
 // METHOD2: replace line
 //    fChain->GetEntry(jentry);       //read all branches
 //by  b_branchname->GetEntry(ientry); //read only this branch
- 
+   if (fChain == 0) return;
 
-  if (fChain == 0) return;
+   Long64_t nentries = fChain->GetEntriesFast();
 
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+   if (fChain == 0) return;
    fChain->SetBranchStatus("pos_pt",1);
    fChain->SetBranchStatus("pos_eta",1);
    fChain->SetBranchStatus("pos_phi",1);
@@ -49,12 +56,22 @@ void K0Lam_study::Loop()
         float  dneg_pt = 0;
         float  dneg_eta= 0;
         float  dneg_phi= 0;
-      TLorentzVector lor_pos;
-      TLorentzVector lor_neg;
-      TLorentzVector lor_sum;
+      TLorentzVector lor_pos ;
+      TLorentzVector lor_neg ;
+      TLorentzVector lor_sum ;
+
+      TLorentzVector lor_posL;
+      TLorentzVector lor_negA;
+      float massK0;
       TH1D * h_massK0;
       h_massK0 = new  TH1D("h_massK0", "h_massK0", 80, 200., 1000.);
-
+      TH1D * h_massLA;
+      h_massLA = new  TH1D("h_massLA", "h_massLA", 80, 700., 1500.);
+      TH2D * h_massK0LA;
+      h_massK0LA = new TH2D("h_massK0LA", "h_massK0LA", 80, 200., 1000., 80,1000., 1800.);
+      TH1D * h_massAL;
+      h_massAL = new  TH1D("h_massAL", "h_massAL", 80, 700., 1500.);
+   std::cout<< " entries " << fChain->GetEntriesFast() << std::endl;
    Long64_t nentries = fChain->GetEntriesFast();
    cout << " nentries = " << nentries << endl;
 
@@ -62,15 +79,17 @@ void K0Lam_study::Loop()
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
-      if( jentry%1000 == 1) {
+       if( jentry%1000 == 1) {
         cout << " process event " << jentry << endl;
        }
+      if( jentry > 16330 && jentry<16350) { std::cout << "evt = " << jentry << " pos->size() = " << pos_pt->size() << std::endl; }
+      if( jentry > 16330 && jentry<16350) continue;
+      
       nb = fChain->GetEntry(jentry);   nbytes += nb;
-      // if (Cut(ientry) < 0) continue;
       if( jentry%1000 == 1) {
                            cout << " jentry = " << jentry <<endl;
                            cout << " pos_pt->size() " << pos_pt->size() << endl;
-                           cout << " pos_pt->at(0) " << pos_pt->at(0) << endl;
+   //                      cout << " pos_pt->at(0) " << pos_pt->at(0) << endl;
                            }
       if( pos_pt->size() !=0) {
                dpos_pt = pos_pt->at(1);
@@ -79,17 +98,34 @@ void K0Lam_study::Loop()
                dneg_pt = neg_pt->at(1);
                dneg_eta= neg_eta->at(1);
                dneg_phi= neg_phi->at(1);
-        if( jentry%1000 == 1) {
-        cout << " pos_pt = " << dpos_pt << endl; }
+//      if( jentry%1000 == 1) {
+//      cout << " pos_pt = " << dpos_pt << endl; }
         lor_pos.SetPtEtaPhiM( dpos_pt, dpos_eta, dpos_phi, mpi);
         lor_neg.SetPtEtaPhiM( dneg_pt, dneg_eta, dneg_phi, mpi);
         lor_sum = lor_pos + lor_neg;
         massK0 = lor_sum.M();
         h_massK0->Fill( massK0 );
+//  Lambda
+        lor_posL.SetPtEtaPhiM( dpos_pt, dpos_eta, dpos_phi, mpp);
+        h_massLA->Fill( (lor_posL + lor_neg).M() );  
+//  antiLambda
+        lor_negA.SetPtEtaPhiM( dneg_pt, dneg_eta, dneg_phi, mpn);
+        h_massAL->Fill( (lor_pos + lor_negA).M() );
+        h_massK0LA->Fill( massK0, (lor_posL + lor_neg).M() );
+      lor_pos.Clear();
+      lor_neg.Clear();
+      lor_sum.Clear();
       }
-   }
+.q   }
+  }
    h_massK0->Draw();
+   h_massLA->Draw();
+   h_massAL->Draw();
+   h_massK0LA->Draw("box");
  TFile *_filewOut= TFile::Open("outfile.root", "recreate");
  h_massK0->Write();
+ h_massLA->Write(); 
+ h_massAL->Write();
+ h_massK0LA->Write();
  _filewOut->Close();
 }
